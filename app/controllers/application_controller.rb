18a30ecef89c5eeb35fedb43a3efeebb4a76b9ea
Matvey Vasilyev <hounsout@yandex.ru>
2024-11-14 08:09:56 +0300

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::API
-    before_action :set_cors_headers
-    private
-
-    def set_cors_headers
-      response.set_header('Access-Control-Allow-Origin', 'https://hounsot.github.io')
-      response.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-      response.set_header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept')
-      response.set_header('Access-Control-Allow-Credentials', 'true')
-    end
 end
