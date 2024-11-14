49086ad0fb790d9ea245040942f07ea62ffcbe9a
Matvey Vasilyev <hounsout@yandex.ru>
2024-11-14 06:55:44 +0300

    Configured hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,12 +77,11 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
-  # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
-  #
+  Enable DNS rebinding protection and other `Host` header attacks.
+  config.hosts = [
+    "zaum.xyz",        # Allow requests from zaum.xyz
+    /.*\.zaum\.xyz/    # Allow requests from subdomains, e.g., www.zaum.xyz
+  ]  #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
